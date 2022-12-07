cdfbf9eba87444f4fd0151001d3e611348cfdf0c
rois abdullah <koregawa10@gmail.com>
2022-12-07 10:51:46 +0700

    update: add some standart animation

--- src/components/Accordion/AccordionGh.js
+++ src/components/Accordion/AccordionGh.js
@@ -4,34 +4,42 @@ import { HiOutlineChevronDown } from "react-icons/hi";
 
 export const AccordionGh = ({data}) => {
   return (
-    <Disclosure>
-    {({ open }) => (
-      <>
-        <Disclosure.Button className="my-2 p-3 rounded-xl bg-slate-300 w-full">
-          <div className='flex items-center justify-between'>
-            <span>
-              {data.owner.login ?? "Without Username"}
-            </span>
-            <HiOutlineChevronDown width={20} className={open ? 'rotate-180 transform' : ''} />
-          </div>
-        </Disclosure.Button>
-          <Disclosure.Panel className="bg-slate-300 p-3 rounded-xl ml-10">
-            <div className="flex justify-between font-extrabold text-lg">
-              <span>
-                {data.title ?? "No Title"}
-              </span>
-              <span>
-                {data.stargazers_count ?? 0}&nbsp;&#9733;
-              </span>
-            </div>
-            <div className='mt-2'>
-              <span>
-                {data.description ?? "No Description"}
-              </span>
-            </div>
-          </Disclosure.Panel>
+    <>
+      {data ? (
+        <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="my-2 p-3 rounded-xl bg-slate-300 w-full">
+              <div className='flex items-center justify-between'>
+                <span>
+                  {data.owner.login ?? "Without Username"}
+                </span>
+                <HiOutlineChevronDown width={20} className={open ? 'rotate-180 transform' : ''} />
+              </div>
+            </Disclosure.Button>
+            <Disclosure.Panel className="bg-slate-300 p-3 rounded-xl ml-10">
+              <div className="flex justify-between font-extrabold text-lg">
+                <span>
+                  {data.title ?? "No Title"}
+                </span>
+                <span>
+                  {data.stargazers_count ?? 0}&nbsp;&#9733;
+                </span>
+              </div>
+              <div className='mt-2'>
+                <span>
+                  {data.description ?? "No Description"}
+                </span>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+        </Disclosure>
+    ) : (
+      <> 
+        <img src="" alt="" />
       </>
     )}
-    </Disclosure>
+    </>
   )
 }
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import {motion} from 'framer-motion'
+import {AnimatePresence, motion} from 'framer-motion'
 import { useDispatch, useSelector } from "react-redux"
 import { AccordionGh } from "../components/Accordion/AccordionGh"
 import { getRepos } from "../helper/redux/Repos/actions"
@@ -18,12 +18,14 @@ export default function Home() {
       <input onChange={(e) => setSearch(e.target.value)} name="searching" type="text" placeholder="Enter Username" className="input input-bordered w-[100%]" />
       <button disabled={!search} onClick={getRepo} className="btn btn-active btn-primary w-full mt-5">Button</button>
         {Repo.repo?.map(item => (
-          <motion.div
-            animate={{y: 30}}
-            initial={{y: 0}}
-          >
-            <AccordionGh data={item} searching={search} />
-          </motion.div>
+          <AnimatePresence>
+            <motion.div
+              animate={{y: 30, opacity: 1, transition: {ease: "easeInOut", duration: 1}}}
+              initial={{y: 0, opacity: 0.1, transition:{ease: "easeInOut", duration: 1}}}
+            >
+              <AccordionGh data={item} searching={search} />
+            </motion.div>
+          </AnimatePresence>
         ))}
     </div>
   )
